aee86b52a20ca13a670e87b115fbe8deb5451ae9
YannisSefsaf <111019910+YannisSefsaf@users.noreply.github.com>
2023-06-14 19:15:59 -0600

    Fix port

--- index.js
+++ index.js
@@ -92,7 +92,8 @@ app.get("/info", (req, res) => {
   `);
 });
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
